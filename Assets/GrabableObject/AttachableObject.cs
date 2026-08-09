d6875932d531eee5080d334e3875c9dc3fc83b6c
vegahouse102 <62749830+vegahouse102@users.noreply.github.com>
2026-08-09 10:14:52 +0900

    Update AttachableObject.cs

--- Assets/GrabableObject/AttachableObject.cs
+++ Assets/GrabableObject/AttachableObject.cs
@@ -8,37 +8,25 @@ public class AttachableObject : MonoBehaviour
 
 	public PieceSO AttachableObjectSO => _AttachableObjectSO;
 
-	public event Action<ChangeTransformDTO> OnChangeTransfrom;
-	private List<AttachableObject> _connectedAttachableObjects = new();
 
-	private Vector3 _prevPosition;
-	private Vector3 _prevRotation;
-	private void OnDestroy()
-	{
-		foreach(var attachedObject in _connectedAttachableObjects)
-		{
-			attachedObject.OnChangeTransfrom -= HandleOnChangeTransform;
-		}
-	}
+	public bool IsAttachable { get; private set; } = true;
 
-	private void FixedUpdate()
+
+	public void SetAttachable(bool value)
 	{
-		
+		IsAttachable = value;
 	}
 	public void AttachObject(AttachableObject otherAttachableObject)
 	{
-		_connectedAttachableObjects.Add(otherAttachableObject);
-		otherAttachableObject.OnChangeTransfrom += HandleOnChangeTransform;
-	}
-	public void HandleOnChangeTransform(ChangeTransformDTO changeDTO)
-	{
+		if (!IsAttachable)
+		{
+			return;
+		}
 
 	}
 
+
+
+
 }
-public struct ChangeTransformDTO
-{
-	public Vector3 DeltaPosition;
-	public Vector3 DeltaRotation;
-	public GameObject ChangedObject;
-}
+
